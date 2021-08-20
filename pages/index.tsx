97d21f20ef753b90e9a0104e0ce4918c35a50950
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-08-20 22:14:42 +0900

    create register function.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,48 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
-export default function Home() {
-  return (
-    <div>Hello.</div>
-  )
+interface Todo {
+  id: number;
+  message: string;
+  // username: string;
+  // avater: string;
 }
+
+const Home = () => {
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (!text) return;
+    const newTodo: Todo = {
+      id: new Date().getTime(),
+      message: text
+      // username: '',
+      // avater: '',
+    }
+    setTodos([...todos, newTodo]);
+    setText('');
+  }
+
+  return (
+    <>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <p>{todo.message}</p>
+          </li>
+        ))}
+      </ul>
+      <form onSubmit={e => handleOnSubmit(e)}>
+        <input
+          type="text"
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
+        <input type="submit" value="追加" onChange={e => handleOnSubmit(e)} />
+      </form>
+    </>
+  );
+}
+
+export default Home;
