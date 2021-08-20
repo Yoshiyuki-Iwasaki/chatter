a4aaf5f00f39fb0bec18b5d0eafe7acedc557901
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-08-21 01:16:05 +0900

    create todoitem components.

---
+++ components/TodoItem.tsx
@@ -0,0 +1,34 @@
+import React, { ReactElement } from "react";
+import { useDocument } from "react-firebase-hooks/firestore";
+import firebase from "../firebase/clientApp";
+
+interface Props {
+  id: number;
+  message: string;
+  userId: string;
+}
+
+const TodoItem = ({ id, message, userId }: Props): ReactElement => {
+  const [value, loading, error] = useDocument(
+    firebase.firestore().doc(`users/${userId}`)
+  );
+
+  if (loading) {
+    return <h6>Loading...</h6>;
+  }
+  if (error) {
+    return null;
+  }
+
+  return (
+    <div key={id}>
+      <img src={value.data().photoURL} alt="" />
+      <h4>{value.data().displayName}</h4>
+      <div>
+        <p>{message}</p>
+      </div>
+    </div>
+  );
+};
+
+export default TodoItem;
--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,34 +1,91 @@
-import React, { ReactElement } from "react";
-import { useDocument } from "react-firebase-hooks/firestore";
 import firebase from "../firebase/clientApp";
+import * as functions from "firebase-functions";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { useState, useEffect } from "react";
+import TodoItem from "./TodoItem";
+import Auth from "./Auth";
 
-interface Props {
+interface Todo {
   id: number;
   message: string;
   userId: string;
 }
 
-const Author = ({ id, message, userId }: Props): ReactElement => {
-  const [value, loading, error] = useDocument(
-    firebase.firestore().doc(`users/${userId}`)
+const TodoList = () => {
+  const db = firebase.firestore();
+  const [user, loading, error] = useAuthState(firebase.auth());
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isChangedTodo, setIsChangedTodo] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [todolists, todolistsLoading, todolistsError] = useCollection(
+    firebase.firestore().collection("chatList"),
+    {}
   );
 
-  if (loading) {
-    return <h6>Loading...</h6>;
-  }
-  if (error) {
-    return null;
+  if (todolistsLoading && todolists) {
+    todolists.docs.map(doc => console.log(doc.data()));
   }
 
+  useEffect(() => {
+    (async () => {
+      const resTodo = await db.collection("chatList").doc("chat").get();
+      setTodos(resTodo.data().chat);
+      setIsLoading(false);
+    })();
+  }, [db]);
+
+  useEffect(() => {
+    if (isChangedTodo) {
+      (async () => {
+        setIsLoading(true);
+        const docRef = await db.collection("chatList").doc("chat");
+        docRef.update({ chat: todos });
+        setIsLoading(false);
+      })();
+    }
+  }, [todos, isChangedTodo, db]);
+
+  const handleOnSubmit = (
+    e: React.FormEvent<HTMLFormElement | HTMLInputElement>
+  ) => {
+    e.preventDefault();
+    if (!text) return;
+    setIsChangedTodo(true);
+    const newTodo: Todo = {
+      id: new Date().getTime(),
+      message: text,
+      userId: user.uid,
+    };
+    setTodos([...todos, newTodo]);
+    setText("");
+  };
   return (
-    <div key={id}>
-      <img src={value.data().photoURL} alt="" />
-      <h4>{value.data().displayName}</h4>
-      <div>
-        <p>{message}</p>
-      </div>
-    </div>
+    <>
+      <ul>
+        {todos &&
+          todos.map((todo, index) => (
+            <ul key={index}>
+              <TodoItem
+                id={todo.id}
+                message={todo.message}
+                userId={todo.userId}
+              />
+            </ul>
+          ))}
+      </ul>
+      <form onSubmit={e => handleOnSubmit(e)}>
+        <input
+          type="text"
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
+        <input type="submit" value="追加" onChange={e => handleOnSubmit(e)} />
+      </form>
+    </>
   );
 };
 
-export default Author;
+export default TodoList;
