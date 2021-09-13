305d2099333286ffce29c6c5f3227db075cd3adc
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-09-13 17:45:33 +0900

    create count up function.

--- src/components/Like.tsx
+++ src/components/Like.tsx
@@ -13,6 +13,7 @@ const Like = ({ postId }) => {
   const db = firebase.firestore();
   const [user, loading, error] = useAuthState(firebase.auth());
   const [done, setDone] = useState(false);
+  const [likeCount, setlikeCount] = useState(0);
   const convertJST = new Date();
   convertJST.setHours(convertJST.getHours());
   const updatedTime = convertJST.toLocaleString("ja-JP").slice(0, -3);
@@ -27,15 +28,27 @@ const Like = ({ postId }) => {
 
   useEffect(() => {
     handleLike();
+    CountLike();
   }, []);
 
+  const CountLike = async () => {
+    await db
+      .collection("likeList")
+      .where("postId", "==", postId)
+      .get()
+      .then(snap => {
+        const size = snap.size;
+        setlikeCount(size);
+      });
+  };
+
   const handleLike = async () => {
-    const citiesRef = await db
+    const likesRef = await db
       .collection("likeList")
       .where("postId", "==", postId)
       .where("userId", "==", user.uid)
       .get();
-    citiesRef.forEach(() => {
+    likesRef.forEach(() => {
       setDone(true);
     });
   };
@@ -48,16 +61,18 @@ const Like = ({ postId }) => {
       createdAt: updatedTime,
     });
     handleLike();
+    CountLike();
   };
 
   const clickRemoveLikeButton = async () => {
-    const citiesRef = await db
+    const likesRef = await db
       .collection("likeList")
       .where("postId", "==", postId)
       .where("userId", "==", user.uid)
       .get();
-    citiesRef.forEach(postDoc => {
+    likesRef.forEach(postDoc => {
       db.collection("likeList").doc(postDoc.id).delete();
+      CountLike();
     });
     setDone(false);
   };
@@ -73,6 +88,7 @@ const Like = ({ postId }) => {
           いいね済み
         </button>
       )}
+      <span className='ml-1'>{likeCount}</span>
     </>
   );
 };
