6e1e866188436a81715964a750ab8358f73b5df4
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-10-02 16:24:50 +0900

    fix splayout.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,19 +8,36 @@ import styled from "styled-components";
 
 const Main = styled.div`
   height: calc(100vh - 58px);
+
+  @media (max-width: 768px) {
+    height: auto;
+  }
 `;
 const Inner = styled.div`
   display: flex;
   width: 100%;
   height: 100%;
+
+  @media (max-width: 768px) {
+    flex-direction: column-reverse;
+  }
 `;
 const SidebarArea = styled.div`
+  margin: 50px 20px 0;
   background: rgba(243, 244, 246, 0.8);
   width: calc(100% / 5);
+
+  @media (max-width: 768px) {
+    width: auto;
+  }
 `;
 const ChatArea = styled.div`
   margin: 0 20px;
   width: calc(100% - (100% / 5));
+
+  @media (max-width: 768px) {
+    width: auto;
+  }
 `;
 
 const Layout = ({ children }: any) => {
